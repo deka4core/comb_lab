87dda912caf5b57f74b92c24753c821d5036740a
deka4core <94462798+deka4core@users.noreply.github.com>
2024-12-14 12:56:34 +0300

    new lab3 без интерфейса

--- main.py
+++ main.py
@@ -1,40 +1,78 @@
-import itertools
+def inverse(permutation):
+    """
+    Вычисляет вектор инверсии по заданной перестановке
+    :param permutation: изначальная перестановка
+    :return: вектор инверсий перестановки
+    """
+    vector_len = len(permutation)
+    inversion_vector = [0] * vector_len
+    bit_tree = [0] * (vector_len + 1)  # Инициализация бинарного дерева нулями. Представление в виде списка
 
-alphabet = [i for i in range(1, 9)]
+    def update(index, val):
+        """
+        Обновляет значение в бинарном дереве отрезков
+        :param index: индекс элемента
+        :param val: добавочное значение
+        :return:
+        """
+        while index <= vector_len:
+            bit_tree[index] += val
+            index += index & -index  # Добавляем к индексу младший бит
+
+    def query(index):
+        """
+        Возвращает сумму элементов в бинарном дереве от 1 до index (Кумулятивная сумма)
+        :param index: индекс элемента
+        :return: кумулятивная сумма
+        """
+        try:
+            res = 0
+            while index > 0:
+                res += bit_tree[index]
+                index -= index & -index
+            return res
+        except IndexError:
+            print("Ошибка входных данных.")
+
+    for i, num in enumerate(permutation):
+        inversion_vector[i] = query(vector_len) - query(num + 1)  # Записывает кол-во чисел больших num справа
+        update(num + 1, 1)  # Добавляем 1 в бинарное дерево отрезков для элемента num
+
+    return inversion_vector
 
 
-def task_1():
-    with open("task_1_output.txt", "w+", encoding='utf-8') as fout:
-        perm = list(itertools.permutations(alphabet, r=5))
-        for item in perm:
-            fout.write("".join(list(map(str, item))) + '\n')
-        fout.write("Кол-во размещений: " + str(len(perm)))
+def recover(inversion_vector):
+    """
+    Восстановление перестановки по вектору инверсий
+    :param inversion_vector: Вектор инверсий
+    :return: Восстановленная перестановка из вектора инверсий
+    """
+    vector_len = len(inversion_vector)
+    permutation = [0] * vector_len
+    items = [x for x in range(vector_len)]  # Множество чисел от 0 до max числа в перестановке
+
+    """
+        Sn - множество перестановок
+        Dn - множество инверсий  
+        Теорема: Отображение Vn: Sn -> Dn является биекцией, причем любая перестановка a, принадлежащая Sn,
+            однозначно восстанавливается по её вектору инверсий Vn(a) 
+    """
+    for i in range(vector_len - 1, -1, -1):  # Перебор вектора инверсий с конца
+        index = inversion_vector[i] + 1
+        permutation[i] = items.pop(-index)
+
+    return permutation
 
 
-def task_2():
-    with open("task_2_output.txt", "w+", encoding='utf-8') as fout:
-        perm = list(itertools.product(alphabet, repeat=7))
-        count = 0
-        for item in perm:
-            c_perm = [item.count(x) for x in item]
-            if (c_perm.count(3) == 3) and (c_perm.count(2) == 2):
-                fout.write("".join(list(map(str, item))) + '\n')
-                count += 1
-        fout.write(f"Кол-во возможных слов длины 7: {count}")
-
-
-def task_3():
-    with open("task_3_output.txt", "w+", encoding='utf-8') as fout:
-        perm = list(itertools.product(alphabet, repeat=6))
-        count = 0
-        for item in perm:
-            if item.count(2) == 3:
-                fout.write("".join(list(map(str, item))) + '\n')
-                count += 1
-        fout.write(f"Кол-во возможных слов длины 6, \nв которых 2 повторяется три раза: {count}")
-
-
-if __name__ == "__main__":
-    task_number = int(input("Введите номер подзадачи: "))
-    tasks = [task_1, task_2, task_3]
-    tasks[task_number - 1]()
+try:
+    """
+        Ввод данных
+    """
+    p = list(map(int, input().split()))
+    if len(set(p)) != len(p):
+        raise ValueError
+    print(f"Перестановка: {p}")
+    print(f"Инверсия: {inverse(p)}")
+    print(f"Восстановленная перестановка: {recover(inverse(p))}")
+except ValueError:
+    print("Некорректный ввод.")
